8c5130c6284010e47f0596738242cac9b7696679
Yoshiaki Numajiri <afmaar128@gmail.com>
2023-07-07 05:07:03 +0900

    ヘッダースタイル途中

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,20 @@
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+jQuery(function ($) {
+	/* ドロワーメニュー開閉 */
+	$("#js-drawer").fadeIn(); // test
 
-
+	// $("#js-menu").click(function () {
+	// 	$("body").toggleClass("is-drawerActive");
+	// 	if ($(this).attr("aria-expanded") == "false") {
+	// 		$(this).attr("aria-expanded", true);
+	// 		$("#js-drawer").attr("aria-hidden", "false");
+	// 		$("#js-drawer").fadeIn();
+	// 		$("html,body").css("overflow", "hidden");
+	// 	} else {
+	// 		$(this).attr("aria-expanded", false);
+	// 		$("#js-drawer").attr("aria-hidden", "true");
+	// 		$("#js-drawer").fadeOut();
+	// 		$("html,body").css("overflow", "initial");
+	// 	}
+	// });
 });
