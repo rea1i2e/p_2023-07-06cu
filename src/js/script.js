2c604de7ccfdb8a0aedda1b1849e0859d530f2ab
Yoshiaki Numajiri <afmaar128@gmail.com>
2023-07-07 09:36:28 +0900

    ヘッダースタイル途中

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,21 @@
-
 jQuery(function ($) {
-	/* ドロワーメニュー開閉 */
-	$("#js-drawer").fadeIn(); // test
+	// $("#js-drawer").fadeIn(); // test
 
-	// $("#js-menu").click(function () {
-	// 	$("body").toggleClass("is-drawerActive");
-	// 	if ($(this).attr("aria-expanded") == "false") {
-	// 		$(this).attr("aria-expanded", true);
-	// 		$("#js-drawer").attr("aria-hidden", "false");
-	// 		$("#js-drawer").fadeIn();
-	// 		$("html,body").css("overflow", "hidden");
-	// 	} else {
-	// 		$(this).attr("aria-expanded", false);
-	// 		$("#js-drawer").attr("aria-hidden", "true");
-	// 		$("#js-drawer").fadeOut();
-	// 		$("html,body").css("overflow", "initial");
-	// 	}
-	// });
+	/* ------------------------------
+ドロワーメニュー開閉（フェード）
+------------------------------ */
+	$("#js-menu").click(function () {
+		$("body").toggleClass("is-drawerActive");
+		if ($(this).attr("aria-expanded") == "false") {
+			$(this).attr("aria-expanded", true);
+			$("#js-drawer").attr("aria-hidden", "false");
+			$("#js-drawer").fadeIn();
+			$("html,body").css("overflow", "hidden");
+		} else {
+			$(this).attr("aria-expanded", false);
+			$("#js-drawer").attr("aria-hidden", "true");
+			$("#js-drawer").fadeOut();
+			$("html,body").css("overflow", "initial");
+		}
+	});
 });
